0a152d6cf4654bda80832df2d56c8c1b46b1fc02
Colin Howe <colinthehowe@gmail.com>
2011-06-10 11:21:28 +0100

    Use save_base instead of save. Commit transactions...

--- modelversions/models.py
+++ modelversions/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.db.models import F, signals
-from django.db import router, connections
+from django.db import router, connections, transaction
 
 class ConcurrentModificationException(Exception):
     pass
@@ -9,7 +9,8 @@ class VersionedModel(models.Model):
     _version = models.IntegerField(null=False, blank=True)
 
 
-    def save(self, force_insert=False, force_update=False, using=None):
+    def save_base(self, raw=False, cls=None, origin=None, force_insert=False, 
+                  force_update=False, using=None):
         '''If this model already exists then this performs an update to ensure
         that the model has not already been updated.'''
         if self._version:
@@ -17,10 +18,13 @@ class VersionedModel(models.Model):
             using = using or router.db_for_write(self.__class__, instance=self)
             connection = connections[using]
             assert not (force_insert and force_update)
-            cls = self.__class__
-            meta = cls._meta
-            if not meta.proxy:
-                origin = cls
+            if cls is None:
+                cls = self.__class__
+                meta = cls._meta
+                if not meta.proxy:
+                    origin = cls
+            else:
+                meta = cls._meta
 
             if origin and not meta.auto_created:
                 signals.pre_save.send(sender=origin, instance=self, raw=False, using=using)
@@ -44,17 +48,16 @@ class VersionedModel(models.Model):
                 if origin and not meta.auto_created:
                     signals.post_save.send(sender=origin, instance=self,
                         created=False, raw=False, using=using)
+                transaction.commit_unless_managed(using=using)
                 return
             else:
                 raise ConcurrentModificationException('Model updated already, was version %d' % self._version)
 
         else:
             self._version = 1
-            return super(VersionedModel, self).save(
-                force_insert=force_insert,
-                force_update=force_update,
-                using=using)
-
+            return super(VersionedModel, self).save_base(
+                raw=raw, cls=cls, origin=origin, force_insert=force_insert, 
+                force_update=force_update, using=using)
 
     class Meta:
         abstract = True
